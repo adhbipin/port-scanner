0fb231ab99d8c614c8ad8ab8e688fdba369a75d7
adhbipin <bipinadh14@gmail.com>
2026-02-16 13:26:07 +0545

    Added start and end port

--- port-scanner.py
+++ port-scanner.py
@@ -2,7 +2,7 @@ import socket
 target = input("Enter IP: ")
 start = int(input("Start port: "))
 end = int(input("End port: "))
-for port in range(1, 1025):
+for port in range(start, end + 1):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.5)
     if sock.connect_ex((target, port)) == 0:
